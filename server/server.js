b45e0e974b5e90272e377742fcf1e7542da4cfd2
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-09-30 22:22:15 +0200

    Basic react app

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
+app.get('/api', (req, res) => {
+    res.send({message: "Hello world"})
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
