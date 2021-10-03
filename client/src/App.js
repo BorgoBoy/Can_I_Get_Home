65ace0b8fab111cac7c214178f8ebd27071dcf37
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-03 17:23:49 +0200

    Basic sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import "../public/tailwind.css"
@@ -7,21 +9,38 @@ import Navbar from "./components/Navbar"
 import LogIn from "./components/LogIn"
 import SignUp from "./components/SignUp"
 import Dashboard from "./components/Dashboard"
+import Settings from "./components/Settings";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBgznEFJwSE7X-O7Hqzad693MxbmAU7jkA",
+    authDomain: "can-i-get-home-6a0de.firebaseapp.com",
+    projectId: "can-i-get-home-6a0de",
+    storageBucket: "can-i-get-home-6a0de.appspot.com",
+    messagingSenderId: "1015685962372",
+    appId: "1:1015685962372:web:795fb4a52d54a4eb5980d1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth()
 
 const App = () => ( 
     <div>
         <Navbar />
         <BrowserRouter>
             <Switch>
-            <Route path="/login">
-                <LogIn />
-            </Route>
-            <Route path="/signup">
-                <SignUp />
-            </Route>
-            <Route path="/dashboard">
-                <Dashboard />
-            </Route>
+                <Route path="/login">
+                    <LogIn auth={auth}/>
+                </Route>
+                <Route path="/signup">
+                    <SignUp auth={auth}/>
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="/settings">
+                    <Settings />
+                </Route>
             </Switch>
         </BrowserRouter>
     </div>
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth"
+
+function register(auth, email, password) {
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+        });
+}
 
 export default class SignUp extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
     render() {
         return (
             <div>
-                <h1>Sign Up</h1>
+            <div className="flex items-center justify-center h-screen">
+            <div className="bg-gray-800 flex flex-col w-80 border border-gray-900 rounded-lg px-10 py-5">
+            <div className="text-white mt-10">
+                <h1 className="font-bold text-4xl">Welcome!</h1>
+                <br />
+                <p className="font-semibold">Let's create your account</p>
+            </div>
+            <form className="flex flex-col space-y-8 mt-10">
+                <input type="text" placeholder="Email" className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"/>
+                <input type="password" placeholder="Password" className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"/>
+                <button className="border border-blue-500 bg-blue-500 text-white rounded-lg py-3 font-semibold" onClick={() => register(this.props.auth, 'a@gmail.com', '123456789')}>Register</button>
+            </form>
+            </div>
+            </div>
             </div>
         )
     }
