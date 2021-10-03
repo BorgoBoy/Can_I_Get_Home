94a83b02e9c7b4a2482010b0b8b95d4d4680783e
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-03 14:37:53 +0200

    Added favicon.png

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./client/public/index.html",
+      favicon: "./client/public/favicon.png"
     }),
     //new WebpackBundleAnalyzer(),
   ],
