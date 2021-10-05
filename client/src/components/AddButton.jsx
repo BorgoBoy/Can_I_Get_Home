670fe8b2919c66811ddb7607421274ceae8ffa75
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-05 16:24:21 +0200

    Added AddButton component

---
+++ client/src/components/AddButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom';
+import { PlusIcon } from "@heroicons/react/solid";
+
+function Add (props) {
+    let history = useHistory()
+
+    return (
+        <div>
+            <button className="absolute bottom-25 right-25 bg-green-500 hover:bg-green-700 py-4 px-4 rounded-full" onClick={() => history.push(props.to)}>
+                <PlusIcon className="h-8 w-8 text-white"/>
+            </button>
+        </div>
+    )
+}
+
+export default Add
