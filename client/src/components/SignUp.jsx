61b07c6acea02601df77b500357a2406d503f672
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-05 15:33:10 +0200

    Basic SignUp component

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { useHistory } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
-function register(auth, email, password) {
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            const user = userCredential.user;
-            console.log(user);
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-        });
-}
+function SignUp (props) {
 
-export default class SignUp extends Component {
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
-        return (
-            <div>
+    let history = useHistory()
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const [user] = useAuthState(props.auth)
+
+    return (
+        <div>
             <div className="flex items-center justify-center h-screen">
             <div className="bg-gray-800 flex flex-col w-80 border border-gray-900 rounded-lg px-10 py-5">
             <div className="text-white mt-10">
@@ -29,14 +21,18 @@ export default class SignUp extends Component {
                 <br />
                 <p className="font-semibold">Let's create your account</p>
             </div>
-            <form className="flex flex-col space-y-8 mt-10">
-                <input type="text" placeholder="Email" className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"/>
-                <input type="password" placeholder="Password" className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"/>
-                <button className="border border-blue-500 bg-blue-500 text-white rounded-lg py-3 font-semibold" onClick={() => register(this.props.auth, 'a@gmail.com', '123456789')}>Register</button>
-            </form>
+            <div className="flex flex-col space-y-8 mt-10 text-white">
+                <input onChange={e => setEmail(e.target.value)} type="text" placeholder="Email" className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"/>
+                <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" className="border rounded-lg py-3 px-3 bg-gray-700 border-gray-700 placeholder-gray-500"/>
+                <button className="border border-blue-500 bg-blue-500 rounded-lg py-3 font-semibold" onClick={() => createUserWithEmailAndPassword(props.auth, email, password).then(history.push('/'))}>Register</button>
             </div>
             </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default SignUp
+
+//TODO: Protect route (user can't access)
+//TODO: Error handling
