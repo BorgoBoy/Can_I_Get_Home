495af47af5aed2fe49f74ffe1dd83888d04b1c7d
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-11 22:21:12 +0200

    Changed firestore schema (not working)

--- client/src/components/NewBike.jsx
+++ client/src/components/NewBike.jsx
@@ -17,24 +17,27 @@ function NewBike(props) {
     const [totalKm, setTotalKm] = useState(0)
 
     return (
-        <div className="w-full text-gray-900 py-36 h-screen"
-            style={{background: `url(${h2r})`}}>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
-                <div className="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-50 p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
-                    <div className="reelative mb-4">
-                        <h1 className="font-medium font-sans text-4xl">Add your motorbike!</h1>
+        <div>
+            {user &&
+            <div className="w-full text-gray-900 py-36 h-screen"
+                style={{background: `url(${h2r})`}}>
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 flex items-center justify-center">
+                    <div className="lg:w-3/6 xl:w-2/5 md:w-full bg-gray-50 p-8 flex flex-col lg:ml-auto w-full mt-10 lg:mt-0 rounded-md">
+                        <div className="reelative mb-4">
+                            <h1 className="font-medium font-sans text-4xl">Add your motorbike!</h1>
+                        </div>
+                        <div className="relative mb-4">
+                            <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Name</label>
+                            <input onChange={e => setName(e.target.value)} type="text" id="name" name="name" className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-0.5 px-3 leading-8 transition-colors duration-150 ease-in-out"/>
+                        </div>
+                        <div className="relative mb-4">
+                            <label htmlFor="totalKm" className="leading-7 text-sm text-gray-600">Total Km</label>
+                            <input onChange={e => setTotalKm(e.target.value)} type="number" id="totalKm" name="totalKm" className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-0.5 px-3 leading-8 transition-colors duration-150 ease-in-out"/>
+                        </div>
+                        <button onClick={() => addDoc(collection(db, toString(user.auth.currentUser.uid + "/bikes")), { name, totalKm }).then(() => history.push('/dashboard'))} className="text-white bg-indigo-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg">Submit</button>
                     </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="full-name" className="leading-7 text-sm text-gray-600">Name</label>
-                        <input onChange={e => setName(e.target.value)} type="text" id="name" name="name" className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-0.5 px-3 leading-8 transition-colors duration-150 ease-in-out"/>
-                    </div>
-                    <div className="relative mb-4">
-                        <label htmlFor="totalKm" className="leading-7 text-sm text-gray-600">Total Km</label>
-                        <input onChange={e => setTotalKm(e.target.value)} type="number" id="totalKm" name="totalKm" className="w-full bg-white rounded-md border border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-indigo-200 text-sm outline-none text-gray-900 py-0.5 px-3 leading-8 transition-colors duration-150 ease-in-out"/>
-                    </div>
-                    <button onClick={() => addDoc(collection(db, "motorbike"), { uid: user.auth.currentUser.uid, name, totalKm }).then(() => history.push('/dashboard'))} className="text-white bg-indigo-500 rounded-md border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 text-lg">Submit</button>
                 </div>
-            </div>
+            </div>}
         </div>
     )
 }
