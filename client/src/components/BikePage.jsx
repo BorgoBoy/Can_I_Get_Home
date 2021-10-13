4ae1942610ec28846786a1c12f5fc5f9b207383e
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-13 21:50:46 +0200

    Not working BikePage component

--- client/src/components/BikePage.jsx
+++ client/src/components/BikePage.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 function BikePage(props) {
+
+    const { id } = useParams()
+
+    const db = getFirestore(props.app);
+    const [bike, setBike] = useState()
+    const [user] = useAuthState(props.auth)
+
+    useEffect(() => {
+        if (user) {
+            async function getBike() {
+                const ref = doc(db, user.auth.currentUser.uid, id)
+                await getDoc(ref).then((doc) => {
+                        setBike(doc.data())
+                })
+            }
+            
+            getBike()
+        }
+    }, [user])
+
     return (
-        <h1>bike page</h1>
+        <div>
+            {user && 
+                <h1>{bike}</h1>
+            }
+        </div>
     )
 }
 
