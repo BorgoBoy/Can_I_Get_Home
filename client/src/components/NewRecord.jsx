3f6ebf9ad8ab43751d71edf6609151e136ad9f07
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-17 17:57:55 +0200

    Added some fields to firebase

--- client/src/components/NewRecord.jsx
+++ client/src/components/NewRecord.jsx
@@ -74,7 +74,7 @@ function NewRecord(props) {
                                         <label htmlFor="totalKm">Enter the total KM:</label>
                                         <input onChange={e => setTotalKM(e.target.value)} id="totalKm" type="number" className="rounded px-4 w-full py-1 bg-gray-100  border border-gray-400 mb-4 text-gray-700 placeholder-gray-700 focus:bg-white focus:outline-none" />
                                         <div className="text-center pt-2">
-                                            <button onClick={() => setDoc(doc(db, user.auth.currentUser.uid, selBike, "records", Date.now().toString()), { bikeData: bikes.find(x => x.id === selBike).data(), liters: parseFloat(liters), totalKm: parseFloat(totalKM) }).then(() => UpdateKM())} className="bg-gray-800 text-gray-200 px-2 py-1 rounded">Add</button>
+                                            <button onClick={() => setDoc(doc(db, user.auth.currentUser.uid, selBike, "records", Date.now().toString()), { bikeData: bikes.find(x => x.id === selBike).data(), liters: parseFloat(liters), totalKm: parseFloat(totalKM), km: parseFloat(totalKM) - parseFloat(bikes.find(x => x.id === selBike).data().totalKm)}).then(() => UpdateKM())} className="bg-gray-800 text-gray-200 px-2 py-1 rounded">Add</button>
                                         </div>
                                     </div>
                                 </div>
