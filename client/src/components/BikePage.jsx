857e31193c5654a1798bce5d407c7892dd5e401a
61619760 <61619760+BorgoBoy@users.noreply.github.com>
2021-10-17 18:32:37 +0200

    Delete button

--- client/src/components/BikePage.jsx
+++ client/src/components/BikePage.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { Line } from 'react-chartjs-2';
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { getFirestore, doc, collection, getDoc, getDocs } from "firebase/firestore";
+import { getFirestore, doc, collection, getDoc, getDocs, deleteDoc } from "firebase/firestore";
 
 import rossi from '/client/public/rossi.jpg'
 import bikeImg from '/client/public/bike.png'
 
 function BikePage(props) {
 
+    let history = useHistory()
     const { id } = useParams()
 
     const db = getFirestore(props.app);
@@ -17,6 +19,8 @@ function BikePage(props) {
 
     const [average, setAverage] = useState([])
     const [km_list, setKm_list] = useState([])
+
+    const [confirm, setConfirm] = useState(false)
 
     const [user] = useAuthState(props.auth)
 
@@ -102,8 +106,11 @@ function BikePage(props) {
                             </div>
                             <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                               <div className="py-6 px-3 mt-32 sm:mt-0">
-                                <button className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
-                                  Connect
+                                <button hidden={confirm} onClick={() => setConfirm(!confirm)} className="bg-red-500 active:bg-red-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+                                  Delete
+                                </button>
+                                <button hidden={!confirm} onClick={() => deleteDoc(doc(db, user.auth.currentUser.uid, id)).then(() => history.push('/dashboard'))} className="bg-black active:bg-black uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150" type="button">
+                                  Are you sure?
                                 </button>
                               </div>
                             </div>
@@ -151,3 +158,4 @@ function BikePage(props) {
 export default BikePage
 
 //TODO: remove useState
+//TODO: delete subcollection
